bb4a770434a79419cfcce8a803fae737962bc098
Carlos Jimenez <carlosmjimenezg@gmail.com>
2024-07-24 13:13:29 -0400

    Text color login

--- src/view/login.js
+++ src/view/login.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import '../index'
-
-
+import '../index';
 
 const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-96">
-        <h1 className="text-4xl font-bold text-center text-gradient bg-gradient-to-r from-pink-500 to-purple-500 mb-8">
+      <div className="bg-white p-8 rounded shadow-md h-96 w-96">
+        <h1 className="text-4xl font-bold text-center bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent mb-8">
           Freeway Airlines
         </h1>
         <form>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            <label className="block text-gray-800 text-sm font-bold mb-2 text-center" htmlFor="password" style={{ color: '#6147FF' }}>
               Accede al sistema
             </label>
             <input
@@ -32,7 +30,7 @@ const Login = () => {
           </div>
           <div className="flex items-center justify-center">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline width:1rem"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-80"
               type="button"
             >
               Ingresar
