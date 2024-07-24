879ef72e892a121a7ce2627f3f82d6ff5192d976
Carlos Jimenez <carlosmjimenezg@gmail.com>
2024-07-24 15:58:32 -0400

    Color text

--- src/view/login.js
+++ src/view/login.js
@@ -4,13 +4,22 @@ import '../index';
 const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md h-96 w-96">
-        <h1 className="text-4xl font-bold text-center bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-transparent mb-8">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl">
+        <h1
+          className="text-5xl font-bold text-center bg-clip-text text-transparent mb-9"
+          style={{
+            backgroundImage: 'linear-gradient(to left, #3D1DFF, #6147FF, #D451FF, #EC458D, #F8938C)',
+          }}
+        >
           Freeway Airlines
         </h1>
         <form>
           <div className="mb-4">
-            <label className="block text-gray-800 text-sm font-bold mb-2 text-center" htmlFor="password" style={{ color: '#6147FF' }}>
+            <label
+              className="block text-gray-800 text-sm font-bold mb-2 text-center mb-9"
+              htmlFor="password"
+              style={{ color: '#6147FF' }}
+            >
               Accede al sistema
             </label>
             <input
@@ -30,8 +39,9 @@ const Login = () => {
           </div>
           <div className="flex items-center justify-center">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-80"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
               type="button"
+              style={{background: '#3D1DFF'}}
             >
               Ingresar
             </button>
