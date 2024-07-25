f1b0ea6a9414b50ea4f0aa24fcd475c286297730
Carlos Jimenez <carlosmjimenezg@gmail.com>
2024-07-25 15:19:36 -0400

    Falta arreglar bugs en filtros

--- src/view/reservas.jsx
+++ src/view/reservas.jsx
@@ -1,69 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import TableAdmin from "../components/datatableAdmin";
 
 const ReservationTable = () => {
+  const [filters, setFilters] = useState({
+    vuelo: "",
+    cedula: "",
+    nombre: "",
+    tipo: "",
+    estado: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFilters({
+      ...filters,
+      [id]: value
+    });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100">
       <div className="bg-white rounded shadow-md w-full">
-        <h1 className="text-4xl font-bold text-center mb-8 text-white p-4"
-        style={{background: "#6147FF"}}>
+        <h1 className="text-4xl font-bold text-center mb-8 text-white p-4" style={{background: "#6147FF"}}>
           RESERVAS
         </h1>
         <form className="mb-8 p-10">
           <div className="flex flex-wrap -mx-2 mb-4">
-            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+            <div className="w-full md:w-1/5 px-2 mb-4 md:mb-0">
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="codigoVuelo"
+                id="vuelo"
                 type="text"
                 placeholder="CÓDIGO DE VUELO"
+                value={filters.vuelo}
+                onChange={handleInputChange}
               />
             </div>
-            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+            <div className="w-full md:w-1/5 px-2 mb-4 md:mb-0">
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="origen"
+                id="cedula"
                 type="text"
-                placeholder="ORIGEN"
+                placeholder="CÉDULA"
+                value={filters.cedula}
+                onChange={handleInputChange}
               />
             </div>
-            <div className="w-full md:w-1/3 px-2">
+            <div className="w-full md:w-1/5 px-2 mb-4 md:mb-0">
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="destino"
+                id="nombre"
                 type="text"
-                placeholder="DESTINO"
+                placeholder="NOMBRE Y APELLIDO"
+                value={filters.nombre}
+                onChange={handleInputChange}
               />
             </div>
-          </div>
-          <div className="flex flex-wrap -mx-2 mb-4">
-            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
+            <div className="w-full md:w-1/5 px-2 mb-4 md:mb-0">
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="fechaSalida"
-                type="date"
-                placeholder="FECHA SALIDA"
-              />
-            </div>
-            <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="fechaRegreso"
-                type="date"
-                placeholder="FECHA REGRESO"
-              />
-            </div>
-            <div className="w-full md:w-1/3 px-2">
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="cedulaPasajero"
+                id="tipo"
                 type="text"
-                placeholder="CÉDULA DEL PASAJERO"
+                placeholder="TIPO"
+                value={filters.tipo}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="w-full md:w-1/5 px-2">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="estado"
+                type="text"
+                placeholder="ESTADO DE LA RESERVA"
+                value={filters.estado}
+                onChange={handleInputChange}
               />
             </div>
           </div>
         </form>
-        <TableAdmin/>
+        <TableAdmin filters={filters} />
       </div>
     </div>
   );
