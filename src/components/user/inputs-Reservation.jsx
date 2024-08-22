1452a6c768beafeb73ee131371273355eb45962d
Carlos Jimenez <carlosmjimenezg@gmail.com>
2024-08-22 14:15:29 -0400

    Conexion al API reservation

--- src/components/user/inputs-Reservation.jsx
+++ src/components/user/inputs-Reservation.jsx
@@ -1,95 +1,58 @@
-import React, { useState } from "react";
-import nacionalidadOptions from '../../.json/nacionalidad.json';
+import React, { useState, useCallback } from "react";
 import Tooltips from "../tooltip";
-import { motion } from 'framer-motion';
-import { Link, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import api from "../../api/api.js";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
 
 const InputsReservation = () => {
   const [formData, setFormData] = useState({
-    nombre: '',
-    apellido: '',
-    fechaNacimiento: '',
-    tipoDocumento: '',
-    numeroDocumento: '',
-    fechaVencimiento: '',
-    paisOrigen: '',
-    genero: '',
-    correo: '',
-    telefono: ''
+    flight_id: "",
+    first_name: "",
+    last_name: "",
+    phone_number: "",
+    cedula: "",
+    status: "SIN CONFIRMAR",
   });
 
   const navigate = useNavigate();
+  const notifySuccess = () => toast.success("Reserva realizada con éxito!");
 
-  const [errors, setErrors] = useState({
-    nombre: '',
-    apellido: '',
-    fechaNacimiento: '',
-    tipoDocumento: '',
-    numeroDocumento: '',
-    fechaVencimiento: '',
-    paisOrigen: '',
-    genero: '',
-    correo: '',
-    telefono: ''
-  });
+  const handleChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }, []);
 
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setFormData({
-      ...formData,
-      [id]: value
-    });
-  };
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      try {
+        const response = await api.post("/reservation", formData);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let valid = true;
-    const newErrors = {
-      nombre: '',
-      apellido: '',
-      fechaNacimiento: '',
-      tipoDocumento: '',
-      numeroDocumento: '',
-      fechaVencimiento: '',
-      paisOrigen: '',
-      genero: '',
-      correo: '',
-      telefono: ''
-    };
-    
-
-    // Validar que el documento comience con V- o E- seguido de números
-    if (!/^([VE]-\d+)$/.test(formData.numeroDocumento)) {
-      newErrors.numeroDocumento = 'El número de documento debe comenzar con V- o E- seguido de números';
-      valid = false;
-    }
-
-    // Validar que el email sea un correo electrónico válido
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(formData.correo)) {
-      newErrors.correo = 'Ingrese un correo electrónico válido';
-      valid = false;
-    }
-
-    // Validar que el teléfono sea numérico
-    if (!/^\d+$/.test(formData.telefono)) {
-      newErrors.telefono = 'El teléfono debe ser numérico';
-      valid = false;
-    }
-
-    setErrors(newErrors);
-
-    if (valid) {
-      console.log('Formulario enviado', formData);
-      navigate("/pagos");
-    }
-  };
+        if (response.status === 201) {
+          notifySuccess();
+          setTimeout(() => {
+            navigate('/pagos'); // Redirige a /pagos después de 3 segundos
+          }, 3000);
+        } else {
+          toast.error(response.data.message || "Error al realizar la reserva.");
+        }
+      } catch (err) {
+        toast.error(err.response?.data?.message || "Error al realizar la reserva.");
+      }
+    },
+    [formData, navigate]
+  );
 
   return (
     <div>
       <Tooltips content="Ingrese los datos en los campos para poder reservar su vuelo" />
       <form onSubmit={handleSubmit}>
-        <motion.div 
+        <motion.div
           className="grid grid-cols-2 gap-4"
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
@@ -98,211 +61,114 @@ const InputsReservation = () => {
           <motion.div className="relative mt-4" transition={{ delay: 0.1 }}>
             <input
               type="text"
-              id="nombre"
+              id="first_name"
+              name="first_name"
               placeholder=" "
               className="peer border p-2 rounded w-full"
-              value={formData.nombre}
+              value={formData.first_name}
               onChange={handleChange}
               required
             />
             <label
-              htmlFor="nombre"
+              htmlFor="first_name"
               className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Nombre*
             </label>
-            {errors.nombre && <p className="text-red-500 text-xs italic">{errors.nombre}</p>}
           </motion.div>
 
           <motion.div className="relative mt-4" transition={{ delay: 0.2 }}>
             <input
               type="text"
-              id="apellido"
+              id="last_name"
+              name="last_name"
               placeholder=" "
               className="peer border p-2 rounded w-full"
-              value={formData.apellido}
+              value={formData.last_name}
               onChange={handleChange}
               required
             />
             <label
-              htmlFor="apellido"
+              htmlFor="last_name"
               className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Apellido*
             </label>
-            {errors.apellido && <p className="text-red-500 text-xs italic">{errors.apellido}</p>}
-          </motion.div>
-
-          <motion.div className="relative mt-4" transition={{ delay: 0.3 }}>
-            <input
-              type="date"
-              id="fechaNacimiento"
-              placeholder=" "
-              className="peer border p-2 rounded w-full"
-              value={formData.fechaNacimiento}
-              onChange={handleChange}
-            />
-            <label
-              htmlFor="fechaNacimiento"
-              className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Fecha Nacimiento
-            </label>
-            {errors.fechaNacimiento && <p className="text-red-500 text-xs italic">{errors.fechaNacimiento}</p>}
-          </motion.div>
-
-          <motion.div className="relative mt-4" transition={{ delay: 0.4 }}>
-            <select
-              id="tipoDocumento"
-              className="peer border p-2 rounded w-full bg-transparent"
-              value={formData.tipoDocumento}
-              onChange={handleChange}
-              required
-            >
-              <option value="" disabled hidden>Tipo de Documento</option>
-              <option value="cedula">Cédula de identidad</option>
-              <option value="pasaporte">Pasaporte</option>
-            </select>
-            <label
-              htmlFor="tipoDocumento"
-              className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Tipo de Documento*
-            </label>
-            {errors.tipoDocumento && <p className="text-red-500 text-xs italic">{errors.tipoDocumento}</p>}
-          </motion.div>
-
-          <motion.div className="relative mt-4" transition={{ delay: 0.5 }}>
-            <input
-              type="text"
-              id="numeroDocumento"
-              placeholder=" "
-              className="peer border p-2 rounded w-full"
-              value={formData.numeroDocumento}
-              onChange={handleChange}
-              required
-            />
-            <label
-              htmlFor="numeroDocumento"
-              className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              N° de Documento*
-            </label>
-            {errors.numeroDocumento && <p className="text-red-500 text-xs italic">{errors.numeroDocumento}</p>}
-          </motion.div>
-
-          <motion.div className="relative mt-4" transition={{ delay: 0.6 }}>
-            <input
-              type="date"
-              id="fechaVencimiento"
-              placeholder=" "
-              className="peer border p-2 rounded w-full"
-              value={formData.fechaVencimiento}
-              onChange={handleChange}
-              required
-            />
-            <label
-              htmlFor="fechaVencimiento"
-              className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Fecha Vto. Pasaporte*
-            </label>
-            {errors.fechaVencimiento && <p className="text-red-500 text-xs italic">{errors.fechaVencimiento}</p>}
-          </motion.div>
-
-          <motion.div className="relative mt-4" transition={{ delay: 0.7 }}>
-            <select
-              id="paisOrigen"
-              className="peer border p-2 rounded w-full bg-transparent"
-              value={formData.paisOrigen}
-              onChange={handleChange}
-            >
-              <option value="" disabled hidden>País de origen</option>
-              {nacionalidadOptions.nacionalidad.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-            <label
-              htmlFor="paisOrigen"
-              className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              País de origen
-            </label>
-            {errors.paisOrigen && <p className="text-red-500 text-xs italic">{errors.paisOrigen}</p>}
-          </motion.div>
-
-          <motion.div className="relative mt-4" transition={{ delay: 0.8 }}>
-            <select
-              id="genero"
-              className="peer border p-2 rounded w-full bg-transparent"
-              value={formData.genero}
-              onChange={handleChange}
-            >
-              <option value="" disabled hidden>Género</option>
-              <option value="masculino">Masculino</option>
-              <option value="femenino">Femenino</option>
-            </select>
-            <label
-              htmlFor="genero"
-              className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Género
-            </label>
-            {errors.genero && <p className="text-red-500 text-xs italic">{errors.genero}</p>}
-          </motion.div>
-
-          <motion.div className="relative mt-4" transition={{ delay: 0.9 }}>
-            <input
-              type="email"
-              id="correo"
-              placeholder=" "
-              className="peer border p-2 rounded w-full"
-              value={formData.correo}
-              onChange={handleChange}
-              required
-            />
-            <label
-              htmlFor="correo"
-              className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Correo*
-            </label>
-            {errors.correo && <p className="text-red-500 text-xs italic">{errors.correo}</p>}
           </motion.div>
 
           <motion.div className="relative mt-4" transition={{ delay: 1.0 }}>
             <input
               type="text"
-              id="telefono"
+              id="phone_number"
+              name="phone_number"
               placeholder=" "
               className="peer border p-2 rounded w-full"
-              value={formData.telefono}
+              value={formData.phone_number}
               onChange={handleChange}
               required
             />
             <label
-              htmlFor="telefono"
+              htmlFor="phone_number"
               className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Teléfono*
             </label>
-            {errors.telefono && <p className="text-red-500 text-xs italic">{errors.telefono}</p>}
+          </motion.div>
+
+          <motion.div className="relative mt-4" transition={{ delay: 0.2 }}>
+            <input
+              type="text"
+              id="cedula"
+              name="cedula"
+              placeholder=" "
+              className="peer border p-2 rounded w-full"
+              value={formData.cedula}
+              onChange={handleChange}
+              required
+            />
+            <label
+              htmlFor="cedula"
+              className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Cedula de identidad*
+            </label>
+          </motion.div>
+
+          <motion.div className="relative mt-4" transition={{ delay: 0.2 }}>
+            <input
+              type="text"
+              id="flight_id"
+              name="flight_id"
+              placeholder=" "
+              className="peer border p-2 rounded w-full"
+              value={formData.flight_id}
+              onChange={handleChange}
+              required
+            />
+            <label
+              htmlFor="flight_id"
+              className="absolute text-gray-500 duration-300 transform -translate-y-6 scale-75 top-1 left-3 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              ID del vuelo*
+            </label>
           </motion.div>
         </motion.div>
 
-        <motion.div 
+        <motion.div
           className="flex justify-center mt-4"
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 1.1 }}
         >
-          <button className="bg-success-color-btn hover:bg-hover-success-color-btn text-white p-2 rounded-lg w-32">
+          <button
+            className="bg-success-color-btn hover:bg-hover-success-color-btn text-white p-2 rounded-lg w-32"
+            type="submit"
+          >
             Guardar
           </button>
         </motion.div>
       </form>
+      <ToastContainer />
     </div>
   );
 };
